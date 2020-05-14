d12cc29ed43c2c2375c25a1cad1e8314012db7b9
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-05-14 14:27:36 -0400

    adding new js file in public folder

--- models/burger.js
+++ models/burger.js
@@ -9,11 +9,11 @@ var burger = {
       });
     },
     // The variables cols and vals are arrays.
-    // create: function(cols, vals, cb) {
-    //   orm.create("cats", cols, vals, function(res) {
-    //     cb(res);
-    //   });
-    // },
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
     // update: function(objColVals, condition, cb) {
     //   orm.update("cats", objColVals, condition, function(res) {
     //     cb(res);
